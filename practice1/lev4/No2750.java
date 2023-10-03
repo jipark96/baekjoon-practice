6643cbf18c17ca216f6013807f04825ca9ee1695
jipark96 <wlgus996@naver.com>
2023-10-03 22:41:09 +0900

    선택, 삽입정렬

--- practice1/lev4/No2750.java
+++ practice1/lev4/No2750.java
@@ -1,5 +1,28 @@
 package practice1.lev4;
 
-public class No2750 {
+import java.util.Scanner;
 
+public class No2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+
+        for(int i = 0; i<N; i++) {
+            A[i] = sc.nextInt();
+        }
+        //[버블 정렬 구현]
+        for (int i=0; i<N-1; i++) {
+            for (int j = 0; j<N-1-i; j++) {
+                if (A[j] > A[j+1]) {
+                    int temp = A[j];
+                    A[j] = A[j+1];
+                    A[j+1] = temp;
+                }
+            }
+        }
+        for (int i=0; i<N; i++) {
+            System.out.println(A[i]);
+        }
+    }
 }
